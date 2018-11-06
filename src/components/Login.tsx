ad41ae4edce9b1bb8079f676e9239641c386ca89
Mahesh Srinivasan <mahesh.srinivasan@spr.com>
2018-11-05 19:02:38 -0600

    "added new file"

---
+++ src/components/Login.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class LoginUI extends Component {
+    render() {
+      return (
+        <div className="App">
+        </div>
+      )      
+    }
+}
